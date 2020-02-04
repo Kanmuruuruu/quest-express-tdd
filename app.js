157d93a2608b64ef3518919e1a15f7976919eb12
Gautier Gueri <gautiergueri@gmail.com>
2020-02-04 15:05:36 +0100

    Realisation de la quete

--- app.js
+++ app.js
@@ -2,14 +2,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+require('dotenv').config();
+const connection = require('./connection');
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req,res)=> {
+app.get('/bookmarks', (req,res)=> {
   res.status(200).send({
     message: "Hello World!"
   });
+});
+
+
+app.get('/bookmarks/:bookmarkId', (req, res) => {
+  connection.query('SELECT * FROM bookmark where id=?', req.params.bookmarkId, (error, result) => {
+      if(result.length===0) return res.status(404).send({ error: 'Bookmark not found' });
+      return res.status(200).send(result[0]);
+  });
+});
+
+
+app.post('/bookmarks', (req, res)=> {
+  const url = req.body.url;
+  const title = req.body.title;
+  if(!url || !title){
+    res.status(422).send({
+      error: "required field(s) missing"
+    })
+  }
+  connection.query('INSERT INTO bookmark SET ?', req.body, (err, stats) => {
+    if (err)
+      return res.status(500).json({ error: err.message, sql: err.sql });
+
+    connection.query('SELECT * FROM bookmark WHERE id = ?', stats.insertId, (err, records) => {
+      if (err)
+        return res.status(500).json({ error: err.message, sql: err.sql });
+      return res.status(201).json(records[0]);
+    });
+  });
+
 });
 
 module.exports = app;
--- test/app.integration.spec.js
+++ test/app.integration.spec.js
@@ -2,10 +2,16 @@
 const request = require('supertest');
 const app = require('../app');
 
+const connection = require('../connection');
+
+/*
 describe('Test routes', () => {
-  it('GET / sends "Hello World" as json', (done) => {
+
+  beforeEach(done => connection.query('TRUNCATE bookmark', done));
+
+  it('GET /bookmarks sends "Hello World" as json', (done) => {
     request(app)
-      .get('/')
+      .get('/bookmarks')
       .expect(200)
       .expect('Content-Type', /json/)
       .then(response => {
@@ -14,4 +20,66 @@ describe('Test routes', () => {
         done();
       });
   });
+
+  it('POST / cant create a bookmark in the database', (done) =>{
+    request(app)
+      .post('/bookmarks')
+      .send({})
+      .expect(422)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = { error: "required field(s) missing"};
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+
+  it('POST / OK fields provided :D', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({"url": "https://jestjs.io", "title": "Jest"})
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .then( response => {
+        const expected = {"id": expect.any(Number),"url": "https://jestjs.io", "title": "Jest"};
+        expect(response.body).toEqual(expected);
+        done();
+      })
+      .catch(done);
+  })
+});
+*/
+
+describe('GET /bookmarks/:id', () => {
+  const testBookmark = { url: 'https://nodejs.org/', title: 'Node.js' };
+  beforeEach((done) => connection.query(
+    'TRUNCATE bookmark', () => connection.query(
+      'INSERT INTO bookmark SET ?', testBookmark, done
+    )
+  ));
+
+  it('GET /bookmarks id not in database', (done) => {
+    request(app)
+      .get('/bookmarks/:bookmarkId')
+      .expect(404)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = { error: 'Bookmark not found' };
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+
+  it('GET /bookmarks/:bookmarkId', (done) => {
+    request(app)
+      .get('/bookmarks/1')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = { id: 1, url: 'https://nodejs.org/', title: 'Node.js' };
+        expect(response.body).toEqual(expected);
+        done();
+      })
+      .catch(done);
+  })
 });
